d48fa85ce7e8201d9a86f8b58be628625d59cdae
Anderson Vieira <anderson@unixtech.com.br>
2017-11-14 15:57:38 -0300

    Update bot.py
    Atualização de regras..

--- bot.py
+++ bot.py
@@ -8,47 +8,41 @@ bot = telebot.TeleBot('token')
 @bot.message_handler(commands=['ajuda', 'help'])
 def comando_help(message):
     bot.reply_to(message,'''
-/link - Exibe o Link de convite para acesso ao grupo
-/regras | /rules - Exibe as regras vigentes no grupo
+/link    - Exibe o Link de convite para acesso ao grupo
+/regras  - Exibe as regras vigentes no grupo
+/ctf     - Exibe o Link do Capture The Flag.
 ''')        
 
 @bot.message_handler(commands=['regras', 'rules'])
 def comando_regras(message):
     bot.reply_to(message,'''
-[*] Grupo de discussão sobre Tecnologia do Sucuri Hacker Club
 
-REGRAS
+[*] Grupo de discussão sobre Security, Hacks e Afins do Sucuri Hacker Club
+
+## REGRAS ##
 
 ======================
-[X] Flood:
-Punição: BAN
+[X] NÃO HÁ REGRAS:
+Punição: USE O BOM SENSO ;-)
 
-[X] Adicionar bots s/ permissão:
-Punição: 1° ocorrência, AVISO
-Punição: 2° ocorrência, BAN
+[x] SE APRESENTE AO PESSOAL NOVATOS!!!
 
-[X] Ficar de zoas com quem está querendo aprender:
-Punição: BAN
-
-[X] Nudes:
-Punição: 1° ocorrência, AVISO
-Punição: 2° ocorrência, BAN
-
-[X] Ofender outros membros:
-Punição: 1° ocorrência, AVISO
-Punição: 2° ocorrência, BAN
 
 ''')
 
 @bot.message_handler(commands=['link'])
 def comando_link(message):
     bot.reply_to(message,'https://telegram.me/joinchat/BSN28wEk-Ffp2yEXjYgx6Q')
+    
+@bot.message_handler(commands=['ctf'])
+def comando_link(message):
+    bot.reply_to(message,'http://ctf.sucurihc.org')
 
 @bot.message_handler(func=lambda mensagem: True, content_types=['new_chat_participant'])
 def mensagem(mensagem):
     bot.reply_to(mensagem, '''
 Bem Vindo ao Grupo Sucuri Hacker Club no Telegram
-Leia as regras antes de postar, envie /regras ou /rules
+Leia as regras antes de postar, envie /regras!!! 
 ''')    
 
 print('Bot Iniciado')        
